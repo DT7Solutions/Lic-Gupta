dc5cdf6393be57eb6abb44ad608072d6f48d02f8
DT7Solutions <developer@dt7solutions.com>
2023-01-07 15:04:09 +0530

    change new event

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,26 +1,23 @@
 # !/bin/bash
 
+if [ -d "env-dinesh" ] 
+then
+    echo "Python virtual environment exists Dinesh Badugu." 
+else
+    python3 -m venv env-dinesh
+fi
 
-python3 -m venv env-dinesh
 source env-dinesh/bin/activate
-# if [ -d "env-dinesh" ] 
-# then
-#     echo "Python virtual environment exists Dinesh Badugu." 
-# else
-#     python3 -m venv env-dinesh
-# fi
-
-# source env-dinesh/bin/activate
 
 
-# pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 
-# if [ -d "logs" ] 
-# then
-#     echo "Log folder exists." 
-# else
-#     mkdir logs
-#     touch logs/error.log logs/access.log
-# fi
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
 
-# sudo chmod -R 777 logs
+sudo chmod -R 777 logs
