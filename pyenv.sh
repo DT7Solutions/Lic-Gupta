f1bffc65be2e5ce3c7c2020d01e55c2f27e422a9
DT7Solutions <developer@dt7solutions.com>
2023-01-07 15:52:50 +0530

    thank god

--- pyenv.sh
+++ pyenv.sh
@@ -13,4 +13,16 @@ source env-dinesh/bin/activate
 
 
 pip3 install -r requirements.txt
-echo'sucessfully activate venv'
+echo "sucessfully activate venv"
+
+
+
+if [ -d "logs" ] 
+then
+    echo "Log folder exists." 
+else
+    mkdir logs
+    touch logs/error.log logs/access.log
+fi
+
+sudo chmod -R 777 logs
